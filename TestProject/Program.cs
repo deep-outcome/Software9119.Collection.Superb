0f61333a1c0d1dfc191fad64491fb0545a4f888a
software9119.technology <Uzivatel919@Seznam.cz>
2026-08-21 14:21:48 +0200

    release version

---
+++ TestProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using Software9119.Collection.Superb.Segmentation;
+
+class Program
+{
+  static void Main ( string [] args )
+  {
+    var segment = new IListSegment<int>([]);
+    var comparer = new IListSegmentEqualityComparer<int>();
+    var enumerator = new IListEnumerator<int>(0, 0, []);
+  }
+}
