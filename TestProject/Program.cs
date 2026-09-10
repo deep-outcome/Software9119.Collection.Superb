7c6e565f092f2b63986e4060ef78b2c841a62e20
software9119.technology <Uzivatel919@Seznam.cz>
2026-09-10 20:52:24 +0200

    post

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -72,6 +72,8 @@ sealed class Program
     IEnumerable<int> sourceOfT = Enumerable.Range(1, 10);
     IEnumerable source = Enumerable.Range(1, 10);
 
+    //source.IntoOrderedDictionary()
+
     // UnsupportedNullBehaviorException
     // Software9119.Collection.Superb.Extension.system_collections.Queue
     //Software9119.Collection.Superb.Extension.AsOrToTargetType
